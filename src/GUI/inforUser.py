259aaf61782f1f64cd38a91699d3f64e36c852c0
NHanh <nha261105@gmail.com>
2025-03-27 23:42:11 +0700

    update inforUser

--- src/GUI/inforUser.py
+++ src/GUI/inforUser.py
@@ -1,29 +1,45 @@
+import os.path
 import tkinter as tk
-import os
-
+from PIL import ImageTk, Image
 
 def northPanel(root):
     northPanel = tk.Frame(root, bg="gray", width=800, height=100)
     northPanel.grid(row=0,padx=10, pady=10,sticky=tk.NSEW)
 
-    icon_add = tk.PhotoImage(file="src/icon/add.png")
-    icon_delete = tk.PhotoImage(file="src/icon/delete.png")
-    icon_detail = tk.PhotoImage(file="src/icon/detail.png")
-    icon_info = tk.PhotoImage(file="src/icon/info.png")
+    imgIcon_add = Image.open("icon/add.png")
+    resize_icon_add = imgIcon_add.resize((20, 20),Image.Resampling.LANCZOS)
+    icon_add = ImageTk.PhotoImage(resize_icon_add)
 
+    imgIcon_delete = Image.open("icon/delete.png")
+    resize_icon_delete = imgIcon_delete.resize((20, 20),Image.Resampling.LANCZOS)
+    icon_delete = ImageTk.PhotoImage(resize_icon_delete)
 
-    them = tk.Button(northPanel,text= "them",image=icon_add)
-    them.pack(side=tk.LEFT,padx=10, pady=10)
+    imgIcon_detail = Image.open("icon/detail.png")
+    resize_icon_detail = imgIcon_detail.resize((20, 20),Image.Resampling.LANCZOS)
+    icon_detail = ImageTk.PhotoImage(resize_icon_detail)
 
-    xoa = tk.Button(northPanel,text= "xoa",image=icon_delete)
-    xoa.pack(side=tk.LEFT,padx=10, pady=10)
+    imgIcon_info = Image.open("icon/add.png")
+    resize_icon_info = imgIcon_info.resize((20, 20),Image.Resampling.LANCZOS)
+    icon_info = ImageTk.PhotoImage(resize_icon_info)
 
-    sua = tk.Button(northPanel,text= "sua",image=icon_detail)
-    sua.pack(side=tk.LEFT,padx=10, pady=10)
+    northPanel.icon_add = icon_add
+    northPanel.icon_delete = icon_delete
+    northPanel.icon_detail = icon_detail
+    northPanel.icon_info = icon_info
 
-    chitiet = tk.Button(northPanel,text= "chi tiet",image=icon_info)
-    chitiet.pack(side=tk.LEFT,padx=10, pady=10)
+    them = tk.Button(northPanel, text="Thêm", image=northPanel.icon_add, compound=tk.LEFT,fg= "gray")
+    them.pack(side=tk.LEFT, padx=10, pady=10)
+
+    xoa = tk.Button(northPanel, text="Xóa", image=northPanel.icon_delete, compound=tk.LEFT,fg= "gray")
+    xoa.pack(side=tk.LEFT, padx=10, pady=10)
+
+    sua = tk.Button(northPanel, text="Sửa", image=northPanel.icon_detail, compound=tk.LEFT,fg = 'gray')
+    sua.pack(side=tk.LEFT, padx=10, pady=10)
+
+    chitiet = tk.Button(northPanel, text="Chi tiết", image=northPanel.icon_info, compound=tk.LEFT,fg = 'gray')
+    chitiet.pack(side=tk.LEFT, padx=10, pady=10)
     return northPanel
+
 def centerPanel(root):
     centerPanel = tk.Frame(root, bg="pink", width=800, height=500)
     centerPanel.grid(row=1, padx=10, pady=10)
@@ -40,6 +56,4 @@ def infor():
     north = northPanel(root)
     center = centerPanel(root)
     root.mainloop()
-# infor()
-
-print(os.path.abspath("src/icon/add.png"))
+infor()
